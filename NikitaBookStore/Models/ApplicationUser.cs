aaa2bbc6ad0be33c93f25418416493e9f83ffbd4
returnaction <obergannv@gmail.com>
2024-03-04 22:48:26 -0500

    Add ApplicationUser Model

---
+++ NikitaBookStore/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace NikitaBookStore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+
+
+    }
+}
